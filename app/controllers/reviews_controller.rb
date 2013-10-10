385b0a199fc4a730d5914e8c122f64b733672466
Handinata Tanudjaja <handi.tan@gmail.com>
2013-10-10 16:43:13 -0700

    Add edit and delete review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,25 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = @movie.reviews.find(params[:id])
+    @review.destroy
+    redirect_to movie_reviews_path(@movie), alert: "Review successfully deleted!"
+  end
+
+  def edit
+    @review = @movie.reviews.find(params[:id])
+  end
+  
+  def update
+    @review = @movie.reviews.find(params[:id])
+    if @review.update(movie_params)
+       redirect_to movie_reviews_path(@movie), notice: "Review successfully edited!" 
+    else
+      render :edit
+    end
+  end
+
   private
     
     def movie_params
